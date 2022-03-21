0f1c6c3a804ed71f12cfae8594fb8d9513dd4126
sulenchy <sulaimanabiodun172@gmail.com>
2022-03-21 22:49:18 +0100

    feat(setup-react): setup react app with some bear components

---
+++ src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from './Footer'
+import Header from './Header'
+
+function app() {
+  return (
+      <>
+        <Header />
+        <div>app</div>
+        <Footer />
+      </>
+  )
+}
+
+export default app
