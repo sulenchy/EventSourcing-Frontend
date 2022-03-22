82f418cd6eec8a88383b1630e1a0f7d2d8836187
sulenchy <sulaimanabiodun172@gmail.com>
2022-03-22 08:53:57 +0100

    chore(hosting): setup the hosting so the api can be available on the remote

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,11 @@ function app() {
   return (
       <>
         <Header />
-        <div>app</div>
+        <video id="background-video" loop autoplay>
+            <source src="http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4" type="video/mp4" />
+            <source src="http://www.sample-videos.com/video/mp4/720/big_buck_bunny_720p_1mb.mp4" type="video/ogg" />
+            Your browser does not support the video tag.
+        </video>
         <Footer />
       </>
   )
--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-
-import App from './components/App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from './theme';
+import App from './components/App';
 
 ReactDOM.render(
-    <App />,
+    <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <App />
+    </ThemeProvider>,
     document.getElementById('root')
 )
